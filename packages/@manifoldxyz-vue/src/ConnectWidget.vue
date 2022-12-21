23ed2bd0694d816326e1b18e0ba816119881035f
Francis John <francis.john@colorado.edu>
2022-12-20 16:32:25 -0800

    fix lint

--- packages/@manifoldxyz-vue/src/ConnectWidget.vue
+++ packages/@manifoldxyz-vue/src/ConnectWidget.vue
@@ -5,7 +5,7 @@ import { camelCaseToHyphenCase, loadConnectWidget } from './shared/utils';
 
 // Can't use defineProps directly imported from another file. Vue is.. interesting.
 // https://github.com/vuejs/core/issues/4294
-interface Props extends ConnectProps { }
+type Props = ConnectProps;
 
 const props = defineProps<Props>();
 
@@ -20,9 +20,6 @@ const propsAsDataAttributes = computed(() => {
 onMounted(() => {
   loadConnectWidget();
 });
-
-
-
 </script>
 <template>
   <div data-widget="m-connect" v-bind="propsAsDataAttributes"></div>
