9b2ef1b395940568e212a2dd795fcc00ead213cb
Francis John <francis.john@colorado.edu>
2022-12-20 16:34:16 -0800

    fix lint rule

--- packages/@manifoldxyz-vue/.eslintrc.js
+++ packages/@manifoldxyz-vue/.eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: ["@manifoldxyz/eslint-config/vue3"],
   rules: {
-    "@typescript-eslint/no-unused-vars": "off"
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/no-empty-interface": "off"
   }
 };
