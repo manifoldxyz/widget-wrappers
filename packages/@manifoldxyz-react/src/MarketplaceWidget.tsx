7f7105f37656e985073e7a22ce9a83312a629864
Stephanie Resis <sresis@umich.edu>
2023-04-10 14:52:07 -0700

    fix

--- packages/@manifoldxyz-react/src/MarketplaceWidget.tsx
+++ packages/@manifoldxyz-react/src/MarketplaceWidget.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import { MarketplaceWidgetDataAttributes } from './shared/types';
 import { Widget } from './Widget';
 
-export const MarketplaceWidget: React.FC<
-  { version?: string } & MarketplaceWidgetDataAttributes
-> = ({ version, ...props }) => {
+export const MarketplaceWidget: React.FC<{ version: string } & MarketplaceWidgetDataAttributes> = ({
+  version,
+  ...props
+}) => {
   return (
     <Widget
       widgetType="marketplace"
