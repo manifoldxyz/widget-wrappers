0e3a76662e1eee2448f7549715808b7efb5a16f5
Stephanie Resis <sresis@umich.edu>
2023-04-10 14:25:29 -0700

    add vue curation widget

---
+++ packages/@manifoldxyz-vue/src/CurationWidget.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+import { CurationWidgetDataAttributes } from './shared/types';
+import Widget from './Widget.vue';
+
+interface Props extends CurationWidgetDataAttributes {
+  version: string;
+}
+const props = defineProps<Props>();
+const dataAttributes = computed(() => {
+  const { version, ...rest } = props;
+
+  return { ...rest };
+});
+</script>
+<template>
+  <Widget :version="props.version" :data-attributes="dataAttributes"></Widget>
+</template>
